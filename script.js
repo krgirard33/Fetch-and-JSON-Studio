11ff231311f4a7563982a366546ca80d70ea712b
krgirard33 <ken.girard@gmail.com>
2022-11-24 22:26:22 -0600

    Finished the main studio. Forgot to make commits during it

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            for(const astronautData of json) {
+                let astronaut = listAstronaut(astronautData)
+                container.innerHTML = container.innerHTML += astronaut;
+            }
+        });
+    });
+});
+
+function listAstronaut(astronautData) {
+    let astronaut = `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronautData.firstName} ${astronautData.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronautData.hoursInSpace}</li>
+                            <li>Active: ${astronautData.active}</li>
+                            <li>Skills: ${astronautData.skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${astronautData.picture}>
+                </div>
+                `
+                return astronaut;
+}
