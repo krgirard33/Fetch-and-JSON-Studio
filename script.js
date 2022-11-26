447339cf67e302d4d916a88eb9aaed4bf390fec2
krgirard33 <ken.girard@gmail.com>
2022-11-25 21:42:31 -0600

    Created activeIsGreen. If astronaut is active, font is capitalized green underlined

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-window.addEventListener("load", function(){
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
-        response.json().then(function(json) {
-            const container = document.getElementById("container");
-            const sorted = json.sort(sortByProperty("hoursInSpace"));
-            for(const astronautData of sorted) {
-                let astronaut = listAstronaut(astronautData)
-                container.innerHTML = container.innerHTML += astronaut;
-            }
-        });
+window.addEventListener("load", function () {
+  fetch(
+    "https://handlers.education.launchcode.org/static/astronauts.json"
+  ).then(function (response) {
+    response.json().then(function (json) {
+      const container = document.getElementById("container");
+      const sorted = json.sort(sortByProperty("hoursInSpace"));
+      for (const astronautData of sorted) {
+        let astronaut = listAstronaut(astronautData);
+        container.innerHTML = container.innerHTML += astronaut;
+      }
+      activeIsGreen();
     });
+  });
 });
 
 function listAstronaut(astronautData) {
-    let astronaut = `
+  let astronaut = `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${astronautData.firstName} ${astronautData.lastName}</h3>
@@ -24,17 +27,28 @@ function listAstronaut(astronautData) {
                     </div>
                     <img class="avatar" src=${astronautData.picture}>
                 </div>
-                `
-                return astronaut;
+                `;
+  return astronaut;
 }
 
 function sortByProperty(property) {
-    return function(a,b){
-        if(a[property]>b[property]){
-            return 1;
-        } else if (a[property]<b[property]){
-            return -1;
-        }
-        return 0;
+  return function (a, b) {
+    if (a[property] > b[property]) {
+      return 1;
+    } else if (a[property] < b[property]) {
+      return -1;
     }
+    return 0;
+  };
+}
+
+function activeIsGreen() {
+  const values = document.querySelectorAll("li");
+  for (const element of values) {
+    if (element.innerText === "Active: true") {
+      element.style.color = "green";
+      element.style.textDecoration = "underline";
+      element.style.textTransform = "capitalize";
+    }
+  }
 }
